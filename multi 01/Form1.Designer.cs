ab541567cdd3b0937105cf31013b9936a72f4595
herqs <herqsss@gmail.com>
2018-06-30 14:55:27 +0300

    FirstCommit

---
+++ multi 01/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace multi_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose dir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(286, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Folders found";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(304, 111);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(644, 212);
+            this.listBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(199, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(954, 111);
+            this.listBox3.MultiColumn = true;
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(188, 212);
+            this.listBox3.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(99, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Enable search";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(16, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Clear tables";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 520);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1120, 37);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 330);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 184);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(304, 330);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(828, 184);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Path";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File";
+            this.columnHeader2.Width = 550;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Folders analysed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 569);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ multi 01/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace multi_01
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog Fbd = new FolderBrowserDialog();
+        int i = 0,ii=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Fbd.ShowDialog() == DialogResult.OK)
+            {
+
+                richTextBox1.AppendText("\r\nLocation locked successfuly");
+                richTextBox1.ScrollToCaret();
+
+                button2.Enabled = true;
+            }
+        }
+
+        private void ListFiles(FolderBrowserDialog Fbd)
+        {
+            String[] Files = Directory.GetFiles(Fbd.SelectedPath);
+
+            /*foreach (string file in Files)
+            {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        listBox1.Items.Add(file);
+                    });
+
+            }*/
+            if (checkBox1.Checked==true)
+            {
+                foreach (string file in Files)
+                {
+                    if (Path.GetFileName(file).Contains(textBox1.Text))
+                    {
+                        object obj = (file);
+                        Thread t3 = new Thread(addfile);
+                        t3.Start(obj);
+                    }
+
+
+                          //  this.BeginInvoke((MethodInvoker)delegate
+                         //   {
+                         //       listBox3.Items.Add(Path.GetFileName(file));
+                         //   });
+                }
+            }
+        }
+
+        private void addfile(object file)
+        {
+            string filestring = file.ToString();
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                ListViewItem item1 = new ListViewItem(Path.GetFileName(filestring));
+                item1.SubItems.Add(Path.GetDirectoryName(filestring));
+                listView1.Items.Add(item1);
+              //  listView1.Items.Add(filestring);1111111
+                listBox3.Items.Add(filestring);
+            });
+
+        }
+
+        private void Godeep(Object obj)
+        {
+            ii++;
+            FolderBrowserDialog Fbd = new FolderBrowserDialog();
+            Fbd.SelectedPath = obj.ToString();
+            ListFiles(Fbd);
+            String[] Dirs = Directory.GetDirectories(Fbd.SelectedPath);
+            FolderBrowserDialog Fbdd= new FolderBrowserDialog();
+            foreach (string dir in Dirs)
+            {
+                Fbdd.SelectedPath = dir;
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                 //   listBox2.Items.Add(dir);
+                });
+                Godeep(Fbdd.SelectedPath);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                textBox1.Enabled = true;
+            else
+                textBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listView1.Items.Clear();
+            i = 0;
+            ii = 0;
+            object obj = Fbd.SelectedPath;
+
+            richTextBox1.AppendText("\r\nStarting folder calculation");
+            richTextBox1.ScrollToCaret();
+
+            CalcFol();
+            label2.Text = i.ToString();
+
+            Thread t1 = new Thread(Godeep);
+            Thread t2 = new Thread(SearchProgress);
+
+            t2.IsBackground = true;
+            t1.IsBackground = true;
+
+            t2.Start();
+            t1.Start(obj);
+
+            richTextBox1.AppendText("\r\nStarting file analyzing process");
+            richTextBox1.ScrollToCaret();
+            if (checkBox1.Checked==true)
+                richTextBox1.AppendText("\r\nLooking for keyword: "+textBox1.Text);
+
+            richTextBox1.AppendText("\r\nStarting progress bar");
+            richTextBox1.ScrollToCaret();
+        }   
+
+      /*  private void CountFolders(string path)
+        {
+            FolderBrowserDialog Fbd = new FolderBrowserDialog();
+            Fbd.SelectedPath = path;
+
+            String[] Dirs = Directory.GetDirectories(Fbd.SelectedPath);
+            FolderBrowserDialog Fbdd = new FolderBrowserDialog();
+
+            foreach (string dir in Dirs)
+            {
+                Fbdd.SelectedPath = dir;
+                i++;
+                
+                CountFolders(Fbdd.SelectedPath);
+            }
+        }*/
+
+        private void CalcFol()
+        {
+            string[] folders = System.IO.Directory.GetDirectories($@"{Fbd.SelectedPath}\", "*", System.IO.SearchOption.AllDirectories);
+            foreach(string asd in folders)
+            {
+                i++;
+            }
+            richTextBox1.AppendText("\r\nFolders calculated successfully, total folders: "+i);
+            richTextBox1.ScrollToCaret();
+            i++;
+        }
+
+
+        private void SearchProgress()
+        {
+            bool go = true;
+            while (go)
+            {
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value = ii / i * 100;
+                    if (ii==i)
+                    {
+                        richTextBox1.AppendText("\r\nAnalyzing complete");
+                        go = false;
+                    }
+                    label3.Text = ii.ToString();
+                });
+                Thread.Sleep(50);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+        }
+    }
+}
